a8cf39cfbb1ed3d4d94dbd59c04f3ecef2fba202
samuel20060921 <98298531+samuel20060921@users.noreply.github.com>
2022-01-24 21:50:56 +0800

    Lesson 1
    Calculating circumference using radius

---
+++ hi.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 20 19:42:05 2022
+
+@author: gamin
+"""
+radius = input('radius of circle:')
+circumference = (float(6.28) * float(radius))
+print('circumference is', circumference)
