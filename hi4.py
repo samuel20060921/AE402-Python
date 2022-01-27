e536759050f0e9146fc0284ea6286cdc49766880
samuel20060921 <98298531+samuel20060921@users.noreply.github.com>
2022-01-27 11:56:11 +0800

    Lesson 4
    Star tower

---
+++ hi4.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 10:25:21 2022
+
+@author: gamin
+"""
+
+for star in range(10,0,-1):
+    print('*' * star)
+    
+star = 10
+while star > 0:
+    print('*' * star)
+    star = star - 1
